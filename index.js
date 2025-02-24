e95e006bc784ff2102ea0e3bef3f41f0ff88a322
ashutoshdas1337 <ashutoshdas1001@gmail.com>
2025-02-24 21:57:50 +0530

    ADD:added functionality to the calculator making it to work properly

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ function buttonClicked(event) {
     if(event.target.textContent==="CLR"){
         screen.textContent=""
     }
+    else if(event.target.textContent==="="){
+        let str=screen.textContent
+// this replace helps to replace all occurences of x to  * 
+        let a=str.replace(/x/g,"*")
+// eval is used inplace of parseInt because parse int converts only the first occurence of a string to int for eg:-parseInt(12+24) then only 12 will be parsed to int and 12 will be parsed to int and 24 will be ignored. so we use eval. however eval is not secure becuase hackers can use it to inject malicous contnent in code 
+ a=eval(a)
+ console.log(a)
+screen.textContent=a
+// screen.textContent=eval(a)
+}
     else{
 
         screen.textContent += event.target.textContent;
